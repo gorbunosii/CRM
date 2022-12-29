085390c15b57ad9cf06e4a94a25f86a9405e7f9a
Denis Gorbunov <Gorbunosii@gmail.com>
2022-12-30 02:32:26 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const productName = 'Наименование товара';
+console.log(productName);
+
+const productAmount = 'Количество товара';
+console.log(productAmount);
+
+const productGroup = 'Категория товара';
+console.log(productGroup);
+
+const productPrice = 'Цена товара';
+console.log(productPrice);
