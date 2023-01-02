17a7583f1f765ae9272205a89f79ff43e854d5d4
gorbunosii <113642114+gorbunosii@users.noreply.github.com>
2023-01-02 16:06:28 +0300

    Add files via upload

---
+++ NEW/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const productName = 'Наименование товара';
+const productAmount = 'Количество товара';
+const productGroup = 'Категория товара';
+const productPrice = 'Цена товара';
+
+
+console.log(productName);
+console.log(productAmount);
+console.log(productGroup);
+console.log(productPrice);
