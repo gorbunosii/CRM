53cd0a56810a4bdb308d7df8a99f8c9e71798c36
Denis Gorbunov <Gorbunosii@gmail.com>
2023-07-10 18:28:00 +0300

    Revert "ready"
    This reverts commit ffab315007506311d1eca803459bf53afc6d13cd.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const crmTitle = document.querySelectorAll(`.title`);
+const crmButtonID = document.querySelectorAll(`.button-funnel`);
+const crmColumnID = document.querySelectorAll(`.ID`);
+const formForm = document.querySelectorAll(`.form`);
+const formCheckbox = document.querySelectorAll(`.checkbox`);
+const formCheckboxInput = document.querySelectorAll(`.checkbox-input`);
+const finalPrice = document.querySelectorAll(`.final-price`);
+
+
+console.log(crmTitle);
+console.log(crmButtonID);
+console.log(crmColumnID);
+console.log(formForm);
+console.log(formCheckbox);
+console.log(formCheckboxInput);
+console.log(finalPrice);
