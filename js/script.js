6574beba1456f5d31bdf2da0de399a9803965e12
Denis Gorbunov <Gorbunosii@gmail.com>
2023-07-18 21:37:53 +0300

    ready

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
 'use strict';
 
-const crmTitle = document.querySelectorAll(`.title`);
-const crmButtonID = document.querySelectorAll(`.button-funnel`);
-const crmColumnID = document.querySelectorAll(`.ID`);
-const formForm = document.querySelectorAll(`.form`);
-const formCheckbox = document.querySelectorAll(`.checkbox`);
-const formCheckboxInput = document.querySelectorAll(`.checkbox-input`);
-const finalPrice = document.querySelectorAll(`.final-price`);
+// const crmTitle = document.querySelectorAll(`.title`);
+// const crmButtonID = document.querySelectorAll(`.button-funnel`);
+// const crmColumnID = document.querySelectorAll(`.ID`);
+// const formForm = document.querySelectorAll(`.form`);
+// const formCheckbox = document.querySelectorAll(`.checkbox`);
+// const formCheckboxInput = document.querySelectorAll(`.checkbox-input`);
+// const finalPrice = document.querySelectorAll(`.final-price`);
 
 
-console.log(crmTitle);
-console.log(crmButtonID);
-console.log(crmColumnID);
-console.log(formForm);
-console.log(formCheckbox);
-console.log(formCheckboxInput);
-console.log(finalPrice);
+const btn = document.querySelector(`.table-add`);
+const formOverlay = document.querySelector(`.overlay`);
+const form = document.querySelector(`.questionnaire`);
+const exit = document.querySelector(`.close`);
+
+btn.addEventListener(`click`, () => {
+  formOverlay.classList.add(`is-visible`);
+  form.classList.add(`is-visible`);
+});
+
+form.addEventListener(`click`, event => {
+  event.stopPropagation();
+});
+
+formOverlay.addEventListener(`click`, () => {
+  formOverlay.classList.remove(`is-visible`);
+  form.classList.remove(`is-visible`);
+});
+
+exit.addEventListener(`click`, () => {
+  formOverlay.classList.remove(`is-visible`);
+  form.classList.remove(`is-visible`);
+});
