5dabe89d2b7d50fa27f514396ec7b331137af674
Denis Gorbunov <Gorbunosii@gmail.com>
2023-07-24 21:47:27 +0300

    проблема с иконкой

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@
 const btn = document.querySelector(`.table-add`);
 const formOverlay = document.querySelector(`.overlay`);
 const form = document.querySelector(`.questionnaire`);
-const exit = document.querySelector(`.close`);
 
 btn.addEventListener(`click`, () => {
   formOverlay.classList.add(`is-visible`);
@@ -11,16 +10,9 @@ btn.addEventListener(`click`, () => {
 });
 
 formOverlay.addEventListener(`click`, e => {
-  // console.log(e.target);
   const target = e.target;
   if (target === formOverlay ||
     target.classList.contains(`close`)) {
     formOverlay.classList.remove(`is-visible`);
-    form.classList.remove(`is-visible`);
   }
 });
-
-// exit.addEventListener(`click`, () => {
-//   formOverlay.classList.remove(`is-visible`);
-//   form.classList.remove(`is-visible`);
-// });
