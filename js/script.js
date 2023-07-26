7d6f58f7395018053da7c56892c088aafebfff59
Denis Gorbunov <Gorbunosii@gmail.com>
2023-07-26 21:27:38 +0300

    chekcbox

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const btnAdd = document.querySelector(`.table-add`);
 const formOverlay = document.querySelector(`.overlay`);
 const form = document.querySelector(`.questionnaire`);
 const btnDel = document.querySelector(`.table-order`);
+const checkboxtBtn = document.querySelector(`.checkbox-label`);
 
 btnAdd.addEventListener(`click`, () => {
   formOverlay.classList.add(`is-visible`);
@@ -22,4 +23,11 @@ btnDel.addEventListener(`click`, e => {
   if (e.target.closest(`.clear`)) {
     e.target.closest(`.order`).remove();
   }
+});
+
+checkboxtBtn.addEventListener(`click`, e => {
+  const checkboxInput = document.querySelector(`.checkbox-input`);
+  e.target.closest(`.checkbox-label`) && checkboxInput.disabled === true ?
+   checkboxInput.disabled = false : checkboxInput.disabled = true;
+  checkboxInput.value = '';
 });
