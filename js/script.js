97c8c93cd016f72e69d7498ee30d32e2c2eabe64
Denis Gorbunov <Gorbunosii@gmail.com>
2023-07-27 00:37:18 +0300

    часть

--- js/script.js
+++ js/script.js
@@ -5,6 +5,54 @@ const formOverlay = document.querySelector(`.overlay`);
 const btnDel = document.querySelector(`.table-order`);
 const checkboxtBtn = document.querySelector(`.checkbox-label`);
 const form = document.querySelector(`.form`);
+const tableTbody = document.querySelector(`.table-order`);
+
+const createRow = ({name, category, unit, amount,
+  price, discount, description, ID}) => {
+  const tr = document.createElement('tr');
+  tr.classList.add(`order`);
+
+  const tdID = document.createElement('td');
+  tdID.classList.add(`td-body`);
+  const spanID = document.createElement(`span`);
+  spanID.classList.add(`vendor-code__id`);
+  spanID.textContent = ID;
+  tdID.append(spanID);
+
+  const tdName = document.createElement('td');
+  tdName.classList.add(`td-body`);
+  tdName.textContent = name;
+
+  const tdCategory = document.createElement('td');
+  tdCategory.classList.add(`td-body`);
+  tdCategory.textContent = category;
+
+  const tdUnit = document.createElement('td');
+  tdUnit.classList.add(`td-body`);
+  tdUnit.textContent = unit;
+
+  const tdAmount = document.createElement('td');
+  tdAmount.classList.add(`td-body`);
+  tdAmount.textContent = amount;
+
+  const tdPrice = document.createElement('td');
+  tdPrice.classList.add(`td-body`);
+  tdPrice.textContent = `$${price}`;
+
+  tr.classList.add(`order`);
+  tr.append(tdID, tdName, tdCategory, tdUnit, tdAmount, tdPrice);
+
+  return tr;
+};
+
+const addContactPage = (contact) => {
+  const randomID = (x, y) => (Math.floor(Math.random() * (y - x)) + x);
+  const ID = randomID(100000000, 999999999);
+  const a = {ID};
+  const obj = Object.assign({}, contact, a);
+  console.log(obj);
+  tableTbody.append(createRow(obj));
+};
 
 btnAdd.addEventListener(`click`, () => {
   formOverlay.classList.add(`is-visible`);
@@ -35,7 +83,7 @@ form.addEventListener(`submit`, e => {
   e.preventDefault();
   const formData = new FormData(e.target);
   const newContact = Object.fromEntries(formData);
-  console.log(newContact);
+  addContactPage(newContact);
   form.reset();
   formOverlay.classList.remove(`is-visible`);
 });
